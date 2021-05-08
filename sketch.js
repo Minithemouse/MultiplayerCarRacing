e050946639bb674ffdfb25e91e58681ed689c39b
Minithemouse <78733670+Minithemouse@users.noreply.github.com>
2021-05-08 16:24:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var database;
+var gamestate = 0;
+var playercount = 0;
+var game, player, form;
+var allplayers;
+var car1, car2, car3, car4;
+var cars;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(displayWidth - 50,displayHeight - 50);
+    
+
+    game = new Game()
+    game.getstate()
+    game.start();
+    
+}
+
+function draw(){
+    background("white");
+
+    if (playercount == 4){
+    game.update(1)
+    }
+
+    if (gamestate == 1){
+        clear()
+        game.play();
+    }
+
+    drawSprites();
+}
